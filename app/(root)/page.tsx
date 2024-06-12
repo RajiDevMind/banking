bf225969c384ab9ae6404bee2f819d4c72298f98
Raji Opeyemi Ibrahim <omoiyaalasso@gmail.com>
2024-06-12 17:19:11 +0100

    Implement the design and display bank cards component on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Raji" };
+  const loggedIn = {
+    firstName: "Raji",
+    lastName: "DevMind",
+    email: "iopeyemi621@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -22,6 +27,14 @@ const Home = () => {
             totalCurrentBalance={1250.43}
           />
         </header>
+        RECENT TRANSACTIONS
+      </div>
+      <div>
+        <RightSideBar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{ currentBalance: 213.21 }, { currentBalance: 545.1 }]}
+        />
       </div>
     </section>
   );
